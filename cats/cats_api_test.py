c9513c15ebdb535cc76b8e60ab982bb66938a81d
Blair Nicolle <29203312+blair-mmai@users.noreply.github.com>
2021-05-05 20:17:36 -0400

    Added extra pandas filter to output
    Just demonstrating pandas filtering syntax.

--- cats/cats_api_test.py
+++ cats/cats_api_test.py
@@ -1,14 +1,16 @@
+# imports installed from pypi.org:
 import requests
-
 import pandas as pd
-from collections import OrderedDict
 
+# imports from python's standard library:
+from collections import OrderedDict
 
 url = 'https://query.wikidata.org/sparql'
 
 # wdt:P31  <--> https://www.wikidata.org/wiki/Property:P31 <--> means: "INSTANCE OF"
 # wd:Q146  <-->  https://www.wikidata.org/wiki/Q146        <--> means: "HOUSE CAT"
 
+# This query is from https://www.wikidata.org/wiki/Wikidata:SPARQL_query_service/queries/examples#Cats
 query = """
 SELECT ?item ?itemLabel 
 WHERE 
@@ -18,11 +20,11 @@ WHERE
 }
 """
 
-
 r = requests.get(url, params = {'format': 'json', 'query': query})
 data = r.json()
 
-print("\n\nHere is our wikidata results in the form of JSON string. It contains the data we want with a bunch of other junk:")
+print("\n\nHere is our wikidata results in the form of JSON string. It contains the data we truly want along with a bunch of other junk:")
+# show the JSON data.
 print(data)
 print("\n")
 
@@ -39,11 +41,18 @@ for this_item in data['results']['bindings']:
 # Convert our python list into a Pandas dataframe named 'df':
 df = pd.DataFrame(cats)
 
-# Very Optional...If we want to remove the ugly index numbering (0,1,2,3, ..) and replace the index with the kitty_name, we can.
+# Very Optional...If we want to remove the 'ugly' numerical indexing (0,1,2,3, ..) and replace the index with the kitty_name, we could.
 # df.set_index('kitty_name', inplace=True)
 
-#show a sampling of our cats from our dataframe.
-print(df.head(5))
+# Show a sampling of our cats from our dataframe.
+print("\nSample of first few cats...")
+df['kitty_lowercase'] = df['kitty_name'].str.lower()
+print(df.loc[df['kitty_lowercase'].str.contains("sock"),['kitty_url','kitty_name']].head(5))
 
-# How many cats do we have in our dataframe
+# Just showing how pandas syntax works for filtering rows and selecting columns.
+print("\nCats with 'sock' in their name")
+df['kitty_lowercase'] = df['kitty_name'].str.lower()
+print(df.loc[df['kitty_lowercase'].str.contains("sock"),['kitty_url','kitty_name']].head(5))
+
+# And, how many cat names do we have in our dataframe, in total?
 print(f"\n\n...We have {len(df)} cats in our dataframe. Oh, yeah.")
